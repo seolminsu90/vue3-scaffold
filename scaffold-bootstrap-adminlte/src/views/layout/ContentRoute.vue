c90918dc3cfb3dbf38eac1188d8fab0d03b208a1
unknown <minsu@nm-neo.com>
2022-07-06 12:39:11 +0900

    update scaffold

---
+++ scaffold-bootstrap-adminlte/src/views/layout/ContentRoute.vue
@@ -0,0 +1,14 @@
+
+<template>
+    <section class="content-wrapper">
+        <router-view />
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'ContentRoute',
+    components: {
+    }
+}
+</script>
